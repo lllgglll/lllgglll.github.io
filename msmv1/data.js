214fbee5960125a164ddb1d9a6dab9ba2e21fe65
lllgglll <49906207+lllgglll@users.noreply.github.com>
2019-05-02 00:58:27 +0300

    Add files via upload

---
+++ msmv1/data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-spalnya_m_pan2",
+      "name": "SPALNYA_M_pan2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": -2.4270115058426924,
+        "pitch": 0.007418868706881909,
+        "fov": 1.088805881264579
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
