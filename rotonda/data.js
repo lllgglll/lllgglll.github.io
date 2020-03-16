473dd3fd5af1017278b1ea9117c23c08de105511
lllgglll <49906207+lllgglll@users.noreply.github.com>
2020-03-16 15:04:13 +0300

    Add files via upload

---
+++ rotonda/data.js
@@ -0,0 +1,131 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-rotv41p",
+      "name": "ROTV41P",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.933843259725105,
+          "pitch": 0.1445871512794774,
+          "rotation": 0,
+          "target": "1-rotv42p"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-rotv42p",
+      "name": "ROTV42P",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9594460689981474,
+          "pitch": 0.24460129013604615,
+          "rotation": 11.780972450961727,
+          "target": "0-rotv41p"
+        },
+        {
+          "yaw": 1.057901411621982,
+          "pitch": 0.27664192214878547,
+          "rotation": 7.0685834705770345,
+          "target": "2-rotv43p"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-rotv43p",
+      "name": "ROTV43P",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0682381879964709,
+          "pitch": 0.22193340582952814,
+          "rotation": 0,
+          "target": "1-rotv42p"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
