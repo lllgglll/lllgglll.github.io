518420ef24c35e50cec2ca86448ff6a82e0ff87c
lllgglll <49906207+lllgglll@users.noreply.github.com>
2020-04-17 02:41:31 +0300

    Add files via upload

---
+++ biliard/data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-rotv14p",
+      "name": "ROTV14P",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 0.02982900797517729,
+        "pitch": -0.07294733969964362,
+        "fov": 0.8191152293480131
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
