62ca0d1f025f256cce27d0f92fa662e68c5b675b
lllgglll <49906207+lllgglll@users.noreply.github.com>
2020-10-26 13:58:13 +0300

    Add files via upload

---
+++ lesv5/data.js
@@ -0,0 +1,131 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-lesv5p2",
+      "name": "LESV5P2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04133381521695689,
+          "pitch": -0.7264939715361756,
+          "rotation": 0,
+          "target": "2-lesv5p3"
+        },
+        {
+          "yaw": 0.0780228745872158,
+          "pitch": 0.49920595022063097,
+          "rotation": 3.141592653589793,
+          "target": "1-lesv5p1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-lesv5p1",
+      "name": "LESV5P1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08355768263669283,
+          "pitch": -0.3766705656630016,
+          "rotation": 0,
+          "target": "0-lesv5p2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-lesv5p3",
+      "name": "LESV5P3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.010694209701789248,
+          "pitch": 0.5667033514908297,
+          "rotation": 9.42477796076938,
+          "target": "0-lesv5p2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
